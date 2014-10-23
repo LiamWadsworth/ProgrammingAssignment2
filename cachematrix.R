2528d7a830ef042f498df5bff5097755731a104e
LiamWadsworth <liam.wadsworth@gmail.com>
2014-10-23 23:27:42 +0100

    Update for assignment
    As required

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R programming assignment two
 
-## Write a short comment describing this function
+## This script has two functions
+## 1) makeCacheMatrix which creates a matrix () and caches it
+## 2) cacheSolve which computes the inverse of matrix () - this is done using the solve function in R
 
-makeCacheMatrix <- function(x = matrix()) {
+## Note: It is assumed in both functions that the matrix supplied will be square
+## Note: Code has been based on the example coding on the assignment page
 
+## 1) makeCacheMatrix which creates a matrix () and caches it
+
+makeCacheMatrix <- function(x = matrix()) { 
+  m <- NULL                    
+  set <- function(y) {
+    x <<- y                      
+    m <<- NULL
+  }
+  
+  get <- function() x			  
+  matrixSet <- function(solve) m <<- solve    
+  matrixGet <- function() m
+  list(set = set, get = get,
+       matrixSet = matrixSet,
+       matrixGet = matrixGet)
 }
 
 
-## Write a short comment describing this function
+
+## 2) cacheSolve which computes the inverse of matrix () - this is done using the solve function in R
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$matrixGet()
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  data <- x$get()
+  inverse <- solve(data, ...)
+  x$matrixSet(inverse)
+  inverse
 }
